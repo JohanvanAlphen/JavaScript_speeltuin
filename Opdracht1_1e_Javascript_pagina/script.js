14b8505420a544ceaa42cb7929316301d27d7f7b
JohanvanAlphen <74180158+JohanvanAlphen@users.noreply.github.com>
2021-01-04 17:13:00 +0100

    Add files via upload

---
+++ Opdracht1_1e_Javascript_pagina/script.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy");
+let wholeName = 'Johan van Alphen';
+console.log(wholeName);
+//Met strings ziet JS dit als tekst
+let additionstring = "4 + 4";
+console.log(additionstring);
+let addition = 4 + 4;
+console.log(addition);
+let substraction = 10 - 3;
+console.log(substraction)
+let multiplication = 5 * 5;
+console.log(multiplication);
+let division = 25 / 5;
+console.log(division);
+let modulus = 12 % 5;
+console.log(modulus);
+let ageNumber = 53;
+console.log(ageNumber);
+let ageText = "53"
+console.log(ageText);
